0e3e64a564fcbccf81acb7fff355e99da5b4e4b2
JasonArriaza <JasonArriaza10@gmail.com>
2023-12-03 23:00:42 -0600

    added more time blocks and have then color coded

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  //add the current day to the top of the page via selecting the current day id in the html
   $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
 
+
+  // event listener when the save btn is clicked and save the data into local storage
   $('.saveBtn').on('click', function () {
-    var timeBlock = $(this).parent();
-    var timeBlockId = timeBlock.attr('id');
-    var userInput = timeBlock.find('.description').val();
-    localStorage.setItem(timeBlockId, userInput);
+    var timeBlock = $(this).parent(); // save the btn parent in its own var
+    var timeBlockId = timeBlock.attr('id'); // target the id in the parent and save in another var
+    var userInput = timeBlock.find('.description').val(); // get the value of the time block
+    localStorage.setItem(timeBlockId, userInput); //save the values in local storage using the made var
   });
 
-$('.time-block').each(function() {
-  var timeBlockId = $(this).attr('id');
-  var userInput = localStorage.getItem(timeBlockId);
 
-  $(this).find('.description').val(userInput);
-});
+  //now display the items from local storage to the blocks
+  $('.time-block').each(function () {
+    var timeBlockId = $(this).attr('id');
+    var userInput = localStorage.getItem(timeBlockId);
+
+    $(this).find('.description').val(userInput);
+  });
+
+  //make code to color code the blocks by the time of day
+  $('.time-block').each(function () {
+    var currentTime = dayjs().hour();
+    var blockTime = parseInt($(this).attr('id').split('-')[1], 10);// convert the string into sub array and get the second item in the array which shoudld contain the number of the hour we want
+    //fuctions to determine the color based on the time of the day, add/remove classes accordingly
+    if (blockTime < currentTime) {
+      $(this).removeClass('present future').addClass('past');
+    } else if (blockTime === currentTime) {
+      $(this).removeClass("past future").addClass('present');
+    } else {
+      $(this).removeClass('past present').addClass('future');
+    }
+
+  })
 
 });
